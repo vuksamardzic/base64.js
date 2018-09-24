34ea17954ddf08437c2944f2b4963e812b0e182e
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-09-24 08:55:59 +0200

    feat: lib source file with proper setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-export const init = (files = []) => {
+import { merge } from './services/merge.service';
+import { options } from './models/options.model';
+import { convert } from './services/convert.service';
 
+export const init = async (files = [], custom_options = {}) => {
+  merge(options, custom_options);
+  return await convert(Array.from(files));
 };
